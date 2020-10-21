37cd556807bc44fd032bcdd2ba1147d78caa9a5f
evanhu1 <hu.evan123@gmail.com>
2020-10-21 14:35:17 -0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,17 +1,71 @@
 import React from 'react';
 import './App.css';
-import {Chess} from 'chess.js';
-// import Chessboard from 'chessboardjs';
+import Chessboard from 'chessboardjsx';
 
-// const chess = new Chess()
-// var board = new Chessboard('board', 'start')
-
-function App() {
-  return (
-    <>
-    <h1>POSITIONAL CHESS TRAINER</h1>
-    </>
-    
-    );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+      this.state = {fen: "", score: "", hideScore: true, picked: false}
+  }
+  componentDidMount() {
+    return fetch("chess-positional-trainer/games.json")
+    .then((response) => response.json())
+      .then((jsonData) => {
+        this.setState({games: jsonData.games})
+        let random = this.getRandomInt(this.state.games.length)
+        this.setState({fen: jsonData.games[random].fen, score:jsonData.games[random].score});
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }
+  getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+  evaluateChoice(choice) {
+    let tempScore = parseInt(this.state.score)/100
+    var answer
+    if (tempScore < -0.5) {
+       answer = -1
+    } else if (tempScore > 0.5) {
+      answer = 1
+    } else {
+      answer = 0
+    }
+    if (choice === answer) {
+      this.setState({picked: true, correct:true})
+    }
+    else {
+      this.setState({picked: true, correct: false})
+    }
+  }
+  render() {
+    console.log(this.state.fen)
+    return (
+      <>
+      <h1>CHESS POSITIONAL TRAINER</h1>
+      <Chessboard calcWidth={({ screenWidth, screenHeight}) => Math.min(0.6*screenWidth, 450)} position={this.state.fen} draggable={false} orientation = {this.state.fen.split(" ")[1] === 'w' ? 'white' : 'black'}/>
+      
+      {this.state.picked ? null :
+      <>
+      <h2>Evaluate the position:</h2>
+      <div className="buttons">
+        <button className="evalButtons" type="button" onClick={() => {this.evaluateChoice(1)}}>White is stronger</button>
+        <button className="evalButtons" type="button" onClick={() => {this.evaluateChoice(0)}}>The position is equal</button>
+        <button className="evalButtons" type="button" onClick={() => {this.evaluateChoice(-1)}}>Black is stronger</button>
+      </div>
+      </>}
+      {this.state.picked ? 
+      <div>
+        <h2>{this.state.correct ? "Correct!" : "Incorrect"}</h2>
+        <p id="score">{"Stockfish evaluation: " + parseInt(this.state.score)/100}</p>
+        <p>{this.state.fen}</p>
+        <button type="button" onClick={() => {let random = this.getRandomInt(this.state.games.length)
+          this.setState({picked:false, hideScore: true, fen: this.state.games[random].fen, score:this.state.games[random].score})}}>Next</button>
+      </div> : null}
+      </>
+      );
+  }
+  
 }
 export default App;
--- src/engine.py
+++ src/engine.py
@@ -4,19 +4,20 @@ from chess import pgn, Board, engine
 from tqdm import tqdm
 import random
 import json
+import pathlib
 
-# stockfish = Stockfish("stockfish")
-stock = engine.SimpleEngine.popen_uci("stockfish")
-# print(stockfish.get_best_move_time(5000))
+stock = engine.SimpleEngine.popen_uci(str(pathlib.Path().absolute())+"/stockfish12")
 board = Board()
-
 raw = open("lichess_elite_2020-09.pgn", encoding="utf-8")
-
 games = {"games": []}
-for i in tqdm(range(10)):
+
+for i in tqdm(range(1000)):
     g = pgn.read_game(raw)
-    length = int(str(g.end())[:2])
-    rand = random.randint(int(length/4), int(3*length/4))
+    try:
+        length = int(str(g.end()).split('.')[0])
+    except ValueError:
+        pass
+    rand = random.randint(int(length/3), int(4*length/5))
     board = g.board()
     for move in g.mainline_moves():
         if rand == 0:
@@ -25,7 +26,11 @@ for i in tqdm(range(10)):
         rand -= 1
     f = board.fen()
     info = stock.analyse(board, engine.Limit(depth=20), info=engine.INFO_SCORE)
-    games["games"].append({"fen": f, "score": int(str(info["score"].relative))})
+    try:
+        games["games"].append({"fen": f, "score": int(str(info["score"].relative))})
+    except ValueError:
+        pass
+    
 
 with open('games.json', 'w') as outfile:
     json.dump(games, outfile)
